7ccfea4a09885a947b3b114c7b41c7720314c4a0
Ali Munir <alimunir.dev@gmail.com>
2026-06-25 23:57:35 +0500

    Added arrays program

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    cout << "Elements of the array are:" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -4,8 +4,5 @@ using namespace std;
 int main()
 {
     cout << "Hello, GitHub!" << endl;
-    cout << "This is my second Git commit." << endl;
-    cout << "Learning Git and GitHub with VS Code." << endl;
-
     return 0;
 }
