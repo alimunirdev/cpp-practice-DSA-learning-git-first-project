d615ec8ca16984dff37881c5fe5a49928ff31225
Ali Munir <alimunir.dev@gmail.com>
2026-06-25 23:43:47 +0500

    Updated main.cpp with new output

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main()
 {
     cout << "Hello, GitHub!" << endl;
+    cout << "This is my second Git commit." << endl;
+    cout << "Learning Git and GitHub with VS Code." << endl;
+
     return 0;
 }
